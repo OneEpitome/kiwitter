5541bdfbba5c013f88e663593fdef99cd503eed8
OneEpitome <rkddlfah0319@gmail.com>
2022-12-26 22:24:01 +0900

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBm_K_89nhXTt_UfW54g0qudiTe08GcpUE",
+  authDomain: "kiwitter-824fa.firebaseapp.com",
+  projectId: "kiwitter-824fa",
+  storageBucket: "kiwitter-824fa.appspot.com",
+  messagingSenderId: "742585381107",
+  appId: "1:742585381107:web:6bd272758833d2486b8786",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
